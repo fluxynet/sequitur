675c237da5eec0a614b747f9d7c8f734bd83e61b
fluxy <fx@fluxy.net>
2019-04-14 23:47:37 +0400

    Refactored Testcases
    Tests restructured for reusability
    Added Error() and LastAction() to Sequence interface
    Allows for generic usage

--- linear.go
+++ linear.go
@@ -2,32 +2,44 @@ package sequitur
 
 //LinearSequence is a group of actions
 type LinearSequence struct {
-	//Error represents any error that has stopped the execution of a sequence
-	Error error
+	//error represents any error that has stopped the execution of a sequence
+	err error
 
-	//LastAction represents the last executed action on the sequence
-	LastAction string
+	//lastAction represents the last executed action on the sequence
+	lastAction string
 
 	//pending indicates that the sequence is still pending, will still accept any do, catch or then
 	pending bool
 }
 
 //Linear returns a new linear sequence
-func Linear() *LinearSequence {
+func Linear() Sequence {
 	return &LinearSequence{pending: true}
 }
 
 func (s *LinearSequence) save(name string, err error) {
-	s.LastAction = name
-	s.Error = err
+	s.lastAction = name
+	s.err = err
+}
+
+//Error returns the error, if any
+func (s *LinearSequence) Error() error {
+	return s.err
+}
+
+//LastAction returns the last executed action including the one that caused the error, if any
+func (s *LinearSequence) LastAction() string {
+	return s.lastAction
 }
 
 //Do executes an action as part of a sequence
 func (s *LinearSequence) Do(name string, action Action) Sequence {
-	if s.pending && s.Error == nil {
+	if s.pending && s.err == nil {
 		var err error
 		defer unpanic(name, s)
-		defer s.save(name, err)
+		defer func() {
+			s.save(name, err)
+		}()
 		err = action()
 	}
 
@@ -36,8 +48,8 @@ func (s *LinearSequence) Do(name string, action Action) Sequence {
 
 //Catch executes a consequence if an error has occurred as part of a sequence
 func (s *LinearSequence) Catch(consequence Consequence) Sequence {
-	if s.pending && s.Error != nil {
-		consequence(s.LastAction, s.Error)
+	if s.pending && s.err != nil {
+		consequence(s.lastAction, s.err)
 		s.pending = false
 	}
 
@@ -46,7 +58,7 @@ func (s *LinearSequence) Catch(consequence Consequence) Sequence {
 
 //Then executes a function if no error has occurred
 func (s *LinearSequence) Then(then func()) Sequence {
-	if s.pending && s.Error == nil {
+	if s.pending && s.err == nil {
 		then()
 		s.pending = false
 	}
